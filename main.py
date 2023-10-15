cfd9bd29812b430118cca9911bf3718f18304e35
iamwonseokchoi <102352229+iamwonseokchoi@users.noreply.github.com>
2023-10-15 14:20:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,68 +35,72 @@ centered_text = '''
     </div>
 '''
 
-# ---------- Main Tab ----------
-with Main: 
-    _, header_col2 = st.columns([1, 1])
+# Access or initialize the session state for the active tab
+if 'active_tab' not in st.session_state:
+    st.session_state.active_tab = None
 
+# ---------- Main Tab ----------
+if Main and st.session_state.active_tab != 'Main':
+    st.session_state.active_tab = 'Main'
+    
+    _, header_col2 = st.columns([1, 1])
     st.markdown("<h1 style='text-align: center; color: #FFA500;'>Generating NASDAQ Alpha</h1>", unsafe_allow_html=True)
     with header_col2: 
         st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
     st.markdown(centered_text, unsafe_allow_html=True)
-    
     # Search company for dashboard view
     search_company()
 
-
 # ---------- Quick Predictions Tab ----------
-with StatsModels:
-    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
+if StatsModels and st.session_state.active_tab != 'StatsModels':
+    st.session_state.active_tab = 'StatsModels'
     
+    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center; color: #FFA500; font-size: 30px;'>Linear Regression & Prophet Forecasting</h1>", unsafe_allow_html=True)
     # Initiate target for prediction
     predict_statsmodels()
 
-
 # ---------- ARIMA Tab ----------
-with Arima:
-    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
+if Arima and st.session_state.active_tab != 'Arima':
+    st.session_state.active_tab = 'Arima'
     
+    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center; color: #FFA500; font-size: 30px;'>ARIMA Forecasting</h1>", unsafe_allow_html=True)
     # Initiate target for prediction
     predict_arima()
 
-
 # ---------- LSTM Tab ----------
-with Lstm:
-    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
+if Lstm and st.session_state.active_tab != 'Lstm':
+    st.session_state.active_tab = 'Lstm'
     
+    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center; color: #FFA500; font-size: 30px;'>LSTM Forecasting</h1>", unsafe_allow_html=True)
     # Initiate target for prediction
     predict_lstm()
 
-
 # ---------- XGBoost Tab ----------
-with Xgboost:
-    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
+if Xgboost and st.session_state.active_tab != 'Xgboost':
+    st.session_state.active_tab = 'Xgboost'
     
+    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center; color: #FFA500; font-size: 30px;'>XGBoost Forecasting</h1>", unsafe_allow_html=True)
     # Initiate target for prediction
     predict_xgboost()
 
-
 # ---------- Optuna LSTM Tab ----------
-with OptunaLstm:
-    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
+if OptunaLstm and st.session_state.active_tab != 'OptunaLstm':
+    st.session_state.active_tab = 'OptunaLstm'
     
+    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center; color: #FFA500; font-size: 30px;'>Auto LSTM</h1>", unsafe_allow_html=True)
     # Initiate target for optuna
     auto_lstm()
-    
 
 # ---------- Optuna XGBoost Tab ----------
-with OptunaXgboost:
-    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
+if OptunaXgboost and st.session_state.active_tab != 'OptunaXgboost':
+    st.session_state.active_tab = 'OptunaXgboost'
     
+    st.markdown(f"<div style='text-align: right;'>{current_time} EST / Market {open_close} {ascii_button}</div>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center; color: #FFA500; font-size: 30px;'>Auto XGBoost</h1>", unsafe_allow_html=True)
     # Initiate target for optuna
     auto_xgb()
